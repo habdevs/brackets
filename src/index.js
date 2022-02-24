0ab849234e4fc693eadcd9b0aead3fc4fcd617a2
Anton <hramickih@yandex.ru>
2022-02-24 18:50:41 +0000

    feat: end task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const bracketArr = bracketsConfig.map(cur => cur.join(''));
+    let bracketStr = str;
+    while (true) {
+        let input = bracketStr;
+        for (let i = 0; i < bracketArr.length; i++) {
+            bracketStr = bracketStr.replace(bracketArr[i], '');
+        }
+        if (input === bracketStr) break;
+    }
+    return !Boolean(bracketStr);
 }
